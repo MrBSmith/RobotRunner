7dbdf01eda2660e1dcbfdb6ee78f1c8b12940189
Weact <l.druckes@ludus-academie.com>
2021-05-10 18:12:28 +0200

    fixed target_level information on confirm menu

--- Scenes/Menus/SaveLoadMenus/SaveConfirmMenu/SaveConfirmMenu.gd
+++ Scenes/Menus/SaveLoadMenus/SaveConfirmMenu/SaveConfirmMenu.gd
@@ -64,8 +64,7 @@ func update_target_save_informations():
 	target_cfg_save_time = GameLoader.get_save_property_value(GAME.SAVE_GAME_DIR, "time", GAME.save_slot)
 	label_tsavename_node.text = get_save_time(target_cfg_save_time)
 
-	label_tsavelevel_node.text += "Level " + str(GameLoader.get_save_property_value(GAME.SAVE_GAME_DIR, "level_id",GAME.save_slot))
-
+	label_tsavelevel_node.text += GAME.current_chapter.get_level_name(GameLoader.get_save_property_value(GAME.SAVE_GAME_DIR, "last_level", GAME.save_slot))
 
 func get_save_time(save_time_dict: Dictionary) -> String:
 	var save_time := ""
