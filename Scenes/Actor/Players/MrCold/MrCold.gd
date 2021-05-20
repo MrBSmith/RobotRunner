8e0467bdfb0db5a0a9548ba24b638fc859dbf14c
Hugo Baba <Hugobarette@gmail.com>
2021-05-20 16:08:46 +0200

    Create classes for each playable Robot

---
+++ Scenes/Actor/Players/MrCold/MrCold.gd
@@ -0,0 +1,26 @@
+extends Player
+class_name MrCold
+
+#### ACCESSORS ####
+
+func is_class(value: String): return value == "MrCold" or .is_class(value)
+func get_class() -> String: return "MrCold"
+
+
+#### BUILT-IN ####
+
+
+
+#### VIRTUALS ####
+
+
+
+#### LOGIC ####
+
+
+
+#### INPUTS ####
+
+
+
+#### SIGNAL RESPONSES ####
---
+++ Scenes/Actor/Players/MrStonks/MrStonks.gd
@@ -0,0 +1,25 @@
+extends Player
+class_name MrStonks
+
+#### ACCESSORS ####
+
+func is_class(value: String): return value == "MrStonks" or .is_class(value)
+func get_class() -> String: return "MrStonks"
+
+#### BUILT-IN ####
+
+
+
+#### VIRTUALS ####
+
+
+
+#### LOGIC ####
+
+
+
+#### INPUTS ####
+
+
+
+#### SIGNAL RESPONSES ####
